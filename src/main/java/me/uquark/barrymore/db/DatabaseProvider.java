43d2d1c2a17aea94a33708195b575e0ad111abf1
Artemii Bigdan <artem@bigdan.in>
2020-03-30 19:21:39 +0300

    Firebird DB and tests

---
+++ src/main/java/me/uquark/barrymore/db/DatabaseProvider.java
@@ -0,0 +1,15 @@
+package me.uquark.barrymore.db;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DatabaseProvider {
+    private static final String DB_URL = "jdbc:firebirdsql://localhost:3050/barrymore?encoding=UTF8";
+    private static final String DB_USER = "barrymore";
+    private static final String DB_PASS = "barrymore";
+
+    public static Connection connect() throws SQLException {
+        return DriverManager.getConnection(DB_URL, DB_USER, DB_PASS);
+    }
+}
---
+++ src/test/java/TestDB.java
@@ -0,0 +1,38 @@
+import me.uquark.barrymore.db.DatabaseProvider;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class TestDB {
+    private Connection connection;
+
+    private void connect() throws SQLException {
+        connection = DatabaseProvider.connect();
+    }
+
+    private void disconnect() throws SQLException {
+        connection.close();
+    }
+
+    @Test
+    public void testConnection() throws SQLException {
+        connect();
+        disconnect();
+    }
+
+    @Test
+    public void testQuery() throws SQLException {
+        connect();
+        Statement statement = connection.createStatement();
+        ResultSet rs = statement.executeQuery("SELECT * FROM TEST");
+        rs.next();
+        Assert.assertEquals(rs.getString(1), "foo");
+        rs.close();
+        statement.close();
+        disconnect();
+    }
+}
